aaca05b3c4b2dfa343472b7038adea98efe75943
Moayadooh <moayadsalim97@gmail.com>
2025-05-17 21:47:23 +0400

    get full object in the response

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -19,11 +19,16 @@ export class AvailablePlacesComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').subscribe({
-      next: (resData) => {
-        console.log(resData);
-      }
-    });
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places', {
+      observe: 'response'
+      //observe: 'events'
+    })
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          console.log(response.body?.places);
+        }
+      });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
